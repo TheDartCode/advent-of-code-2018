b6590751a1570cdb2a5e5a5a7335da0f17dc36df
TheDartCode <giorgos@thedartcode.com>
2018-12-18 15:32:42 +0200

    Solve Day11:PuzzleA

--- src/day11/__tests__.js
+++ src/day11/__tests__.js
@@ -1,21 +1,82 @@
 const {expect} = require('chai');
 import {
+  getPowerLevel,
+  setupGrid,
+  calculateGridRegions,
   moduleA,
   moduleB,
 } from './';
+import {numericMax} from '../shared/helpers';
+
+const sliceRegion = (grid, x, y, width, height) => {
+  return grid.slice(y - 1, y - 1 + height).map(row => row
+    .slice(x - 1, x - 1 + width)
+    .map(cell => cell.powerLevel));
+};
 
 describe('Day 11', () => {
-  const input = '8';
+  const input = '18';
+
+  describe('helpers', () => {
+    describe('#getPowerLevel', () => {
+      it('calculates the power level for a given cell and grid serial number', () => {
+        expect(getPowerLevel({x: 3, y: 5}, 8)).to.eq(4);
+        expect(getPowerLevel({x: 122, y: 79}, 57)).to.eq(-5);
+        expect(getPowerLevel({x: 217, y: 196}, 39)).to.eq(0);
+        expect(getPowerLevel({x: 101, y: 153}, 71)).to.eq(4);
+      });
+    });
+
+    describe('#setupGrid', () => {
+      it('creates the 300x300 grid assigning power levels to cells', () => {
+        let grid = setupGrid(18);
+        let region = sliceRegion(grid, 33 - 1, 45 - 1, 5, 5);
+        expect(region).to.deep.eq([
+          [-2, -4, 4, 4, 4],
+          [-4, 4, 4, 4, -5],
+          [4, 3, 3, 4, -4],
+          [1, 1, 2, 4, -3],
+          [-1, 0, 2, -5, -2],
+        ]);
+
+        grid = setupGrid(42);
+        region = sliceRegion(grid, 21 - 1, 61 - 1, 5, 5);
+        expect(region).to.deep.eq([
+          [-3, 4, 2, 2, 2],
+          [-4, 4, 3, 3, 4],
+          [-5, 3, 3, 4, -4],
+          [4, 3, 3, 4, -3],
+          [3, 3, 3, -5, -1],
+        ]);
+      });
+    });
+
+    describe('#calculateGridRegions', () => {
+      it('calculates all possible 3x3 regions with their corresponding power level', () => {
+        let grid = setupGrid(18);
+        let regions = calculateGridRegions(grid);
+        expect(regions.length).to.eq(88804);
+        expect(regions.find(region => region.x === 33 && region.y === 45).totalPower).to.eq(29);
+        expect(numericMax(regions, region => region.totalPower)).to.eq(29);
+
+        grid = setupGrid(42);
+        regions = calculateGridRegions(grid);
+        expect(regions.length).to.eq(88804);
+        expect(regions.find(region => region.x === 21 && region.y === 61).totalPower).to.eq(30);
+        expect(numericMax(regions, region => region.totalPower)).to.eq(30);
+      });
+    });
+  });
 
   describe('Puzzle A', () => {
     it('satisfies examples', () => {
-      expect(moduleA(input)).to.eq();
+      expect(moduleA(input)).to.eq('33,45');
     });
   });
 
   describe('Puzzle B', () => {
     it('satisfies examples', () => {
-      expect(moduleB(input)).to.eq();
+      expect(moduleB(input)).to.eq(18);
     });
   });
 
--- src/day11/index.js
+++ src/day11/index.js
@@ -1,7 +1,68 @@
+import {range} from '../shared/helpers';
+
 const parseInput = input => parseInt(input.trim(), 10);
 
+const GRID_WIDTH = 300;
+const GRID_HEIGHT = 300;
+
+const getPowerLevel = ({x, y}, gridSerial) => {
+  const rackID = x + 10;
+  let powerLevel = rackID * y;
+  powerLevel = powerLevel + gridSerial;
+  powerLevel = powerLevel * rackID;
+  powerLevel = Math.floor(powerLevel / 100) % 10;
+  return powerLevel - 5;
+};
+
+const setupGrid = gridSerial => {
+  return range(GRID_HEIGHT)
+    .map(yIndex => range(GRID_WIDTH)
+      .map(xIndex => {
+        const cell = {
+          x: xIndex + 1,
+          y: yIndex + 1,
+        };
+        cell.powerLevel = getPowerLevel(cell, gridSerial);
+        return cell;
+      })
+    );
+};
+
+const calculateGridRegions = grid => {
+  const regions = [];
+
+  grid.forEach(row => {
+    row.forEach(cell => {
+      if (cell.x + 2 > GRID_WIDTH || cell.y + 2 > GRID_WIDTH) {
+        return;
+      }
+      let totalPower = 0;
+      for (let j = cell.y - 1; j < cell.y - 1 + 3; j = j + 1) {
+        for (let i = cell.x - 1; i < cell.x - 1 + 3; i = i + 1) {
+          totalPower = totalPower + grid[j][i].powerLevel;
+        }
+      }
+      regions.push({
+        x: cell.x,
+        y: cell.y,
+        totalPower,
+      });
+    });
+  });
+
+  return regions;
+};
+
 const moduleA = input => {
-  return parseInput(input);
+  const gridSerial = parseInput(input);
+
+  const grid = setupGrid(gridSerial);
+
+  const regions = calculateGridRegions(grid);
+
+  const highestPowerRegion = regions.sort((r1, r2) => r2.totalPower - r1.totalPower)[0];
+
+  return `${highestPowerRegion.x},${highestPowerRegion.y}`;
 };
 
 const moduleB = input => {
@@ -9,6 +70,9 @@ const moduleB = input => {
 };
 
 export {
+  getPowerLevel,
+  setupGrid,
+  calculateGridRegions,
   moduleA,
   moduleB,
 };
