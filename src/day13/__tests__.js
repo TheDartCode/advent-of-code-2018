265917240683249a2bbb74ca0e1784d758fd3446
TheDartCode <giorgos@thedartcode.com>
2018-12-22 20:48:33 +0200

    Solve Day13:PuzzleA

--- src/day13/__tests__.js
+++ src/day13/__tests__.js
@@ -1,21 +1,166 @@
 const {expect} = require('chai');
 import {
+  Cart,
+  parseInput,
+  tick,
   moduleA,
   moduleB,
 } from './';
+import {
+  CART_DIRECTIONS,
+  MAP_TILES,
+  INTERSECTION_CHOICES,
+} from './constants';
 
 describe('Day 13', () => {
-  const input = '';
+  const input = [
+    '/->-\\        ',
+    '|   |  /----\\',
+    '| /-+--+-\\  |',
+    '| | |  | v  |',
+    '\\-+-/  \\-+--/',
+    '  \\------/   ',
+  ].join('\n');
+
+  const carts = [
+    {x: 2, y: 0, direction: CART_DIRECTIONS.RIGHT, lastChoice: null},
+    {x: 9, y: 3, direction: CART_DIRECTIONS.DOWN, lastChoice: null},
+  ];
+
+  const map = [
+    ['/', '-', '-', '-', '\\', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    ['|', ' ', ' ', ' ', '|', ' ', ' ', '/', '-', '-', '-', '-', '\\'],
+    ['|', ' ', '/', '-', '+', '-', '-', '+', '-', '\\', ' ', ' ', '|'],
+    ['|', ' ', '|', ' ', '|', ' ', ' ', '|', ' ', '|', ' ', ' ', '|'],
+    ['\\', '-', '+', '-', '/', ' ', ' ', '\\', '-', '+', '-', '-', '/'],
+    [' ', ' ', '\\', '-', '-', '-', '-', '-', '-', '/', ' ', ' ', ' '],
+  ];
+
+  describe('Cart', () => {
+    describe('#getNextChoice', () => {
+      it('returns the turn for the next intersection', () => {
+        const cart = new Cart();
+        cart.getNextChoice();
+        expect(cart.lastChoice).to.eq(INTERSECTION_CHOICES.LEFT);
+        cart.getNextChoice();
+        expect(cart.lastChoice).to.eq(INTERSECTION_CHOICES.STRAIGHT);
+        cart.getNextChoice();
+        expect(cart.lastChoice).to.eq(INTERSECTION_CHOICES.RIGHT);
+        cart.getNextChoice();
+        expect(cart.lastChoice).to.eq(INTERSECTION_CHOICES.LEFT);
+      });
+    });
+
+    describe('#getNextPosition', () => {
+      it('returns the position after the next tick, depending on the direction', () => {
+        const cart = new Cart(0, 0, CART_DIRECTIONS.UP);
+        expect(cart.getNextPosition()).to.deep.eq({x: 0, y: -1});
+        cart.direction = CART_DIRECTIONS.RIGHT;
+        expect(cart.getNextPosition()).to.deep.eq({x: 1, y: 0});
+        cart.direction = CART_DIRECTIONS.DOWN;
+        expect(cart.getNextPosition()).to.deep.eq({x: 0, y: 1});
+        cart.direction = CART_DIRECTIONS.LEFT;
+        expect(cart.getNextPosition()).to.deep.eq({x: -1, y: 0});
+        cart.direction = 'invalid';
+        expect(cart.getNextPosition).to.throw();
+      });
+    });
+
+    describe('#getNextDirection', () => {
+      it('returns the direction after visiting the given tile', () => {
+        let cart = new Cart(0, 0, CART_DIRECTIONS.UP);
+        expect(cart.getNextDirection(MAP_TILES.VERTICAL)).to.eq(CART_DIRECTIONS.UP);
+        expect(cart.getNextDirection(MAP_TILES.DIAGONAL_SLASH)).to.eq(CART_DIRECTIONS.RIGHT);
+        expect(cart.getNextDirection(MAP_TILES.DIAGONAL_BACKSLASH)).to.eq(CART_DIRECTIONS.LEFT);
+        expect(cart.getNextDirection(MAP_TILES.INTERSECTION)).to.eq(CART_DIRECTIONS.LEFT);
+        expect(cart.getNextDirection(MAP_TILES.INTERSECTION)).to.eq(CART_DIRECTIONS.UP);
+        expect(cart.getNextDirection(MAP_TILES.INTERSECTION)).to.eq(CART_DIRECTIONS.RIGHT);
+        expect(() => cart.getNextDirection(MAP_TILES.HORIZONTAL)).to.throw();
+
+        cart = new Cart(0, 0, CART_DIRECTIONS.DOWN);
+        expect(cart.getNextDirection(MAP_TILES.VERTICAL)).to.eq(CART_DIRECTIONS.DOWN);
+        expect(cart.getNextDirection(MAP_TILES.DIAGONAL_SLASH)).to.eq(CART_DIRECTIONS.LEFT);
+        expect(cart.getNextDirection(MAP_TILES.DIAGONAL_BACKSLASH)).to.eq(CART_DIRECTIONS.RIGHT);
+        expect(cart.getNextDirection(MAP_TILES.INTERSECTION)).to.eq(CART_DIRECTIONS.RIGHT);
+        expect(cart.getNextDirection(MAP_TILES.INTERSECTION)).to.eq(CART_DIRECTIONS.DOWN);
+        expect(cart.getNextDirection(MAP_TILES.INTERSECTION)).to.eq(CART_DIRECTIONS.LEFT);
+        expect(() => cart.getNextDirection(MAP_TILES.HORIZONTAL)).to.throw();
+
+        cart = new Cart(0, 0, CART_DIRECTIONS.LEFT);
+        expect(cart.getNextDirection(MAP_TILES.HORIZONTAL)).to.eq(CART_DIRECTIONS.LEFT);
+        expect(cart.getNextDirection(MAP_TILES.DIAGONAL_SLASH)).to.eq(CART_DIRECTIONS.DOWN);
+        expect(cart.getNextDirection(MAP_TILES.DIAGONAL_BACKSLASH)).to.eq(CART_DIRECTIONS.UP);
+        expect(cart.getNextDirection(MAP_TILES.INTERSECTION)).to.eq(CART_DIRECTIONS.DOWN);
+        expect(cart.getNextDirection(MAP_TILES.INTERSECTION)).to.eq(CART_DIRECTIONS.LEFT);
+        expect(cart.getNextDirection(MAP_TILES.INTERSECTION)).to.eq(CART_DIRECTIONS.UP);
+        expect(() => cart.getNextDirection(MAP_TILES.VERTICAL)).to.throw();
+
+        cart = new Cart(0, 0, CART_DIRECTIONS.RIGHT);
+        expect(cart.getNextDirection(MAP_TILES.HORIZONTAL)).to.eq(CART_DIRECTIONS.RIGHT);
+        expect(cart.getNextDirection(MAP_TILES.DIAGONAL_SLASH)).to.eq(CART_DIRECTIONS.UP);
+        expect(cart.getNextDirection(MAP_TILES.DIAGONAL_BACKSLASH)).to.eq(CART_DIRECTIONS.DOWN);
+        expect(cart.getNextDirection(MAP_TILES.INTERSECTION)).to.eq(CART_DIRECTIONS.UP);
+        expect(cart.getNextDirection(MAP_TILES.INTERSECTION)).to.eq(CART_DIRECTIONS.RIGHT);
+        expect(cart.getNextDirection(MAP_TILES.INTERSECTION)).to.eq(CART_DIRECTIONS.DOWN);
+        expect(() => cart.getNextDirection(MAP_TILES.VERTICAL)).to.throw();
+      });
+    });
+  });
+
+  describe('helpers', () => {
+    const cartToPOJO = cart => ({
+      x: cart.x,
+      y: cart.y,
+      direction: cart.direction,
+      crash: cart.crash || false,
+    });
+
+    describe('#parseInput', () => {
+      it('parses carts and tracks from input map', () => {
+        expect(parseInput(input)).to.deep.eq({
+          carts,
+          map,
+        });
+      });
+    });
+
+    describe('#tick', () => {
+      it('advances all carts one step, respecting priority, and sets their :crash property', () => {
+        expect(tick([
+          new Cart(2, 0, CART_DIRECTIONS.RIGHT),
+          new Cart(9, 3, CART_DIRECTIONS.DOWN),
+        ], map).map(cartToPOJO)).to.deep.eq([
+          {x: 3, y: 0, direction: CART_DIRECTIONS.RIGHT, crash: false},
+          {x: 9, y: 4, direction: CART_DIRECTIONS.RIGHT, crash: false},
+        ]);
+        expect(tick([
+          new Cart(3, 0, CART_DIRECTIONS.LEFT),
+          new Cart(2, 0, CART_DIRECTIONS.LEFT),
+        ], map).map(cartToPOJO)).to.deep.eq([
+          {x: 1, y: 0, direction: CART_DIRECTIONS.LEFT, crash: false},
+          {x: 2, y: 0, direction: CART_DIRECTIONS.LEFT, crash: false},
+        ]);
+        expect(tick([
+          new Cart(2, 0, CART_DIRECTIONS.RIGHT),
+          new Cart(4, 0, CART_DIRECTIONS.LEFT),
+        ], map).map(cartToPOJO)).to.deep.eq([
+          {x: 3, y: 0, direction: CART_DIRECTIONS.RIGHT, crash: true},
+          {x: 3, y: 0, direction: CART_DIRECTIONS.LEFT, crash: true},
+        ]);
+      });
+    });
+
+  });
 
   describe('Puzzle A', () => {
     it('satisfies examples', () => {
-      expect(moduleA(input)).to.eq();
+      expect(moduleA(input)).to.eq('7,3');
     });
   });
 
   describe('Puzzle B', () => {
     it('satisfies examples', () => {
-      expect(moduleB(input)).to.eq();
+      // expect(moduleB(input)).to.eq();
     });
   });
 
--- src/day13/index.js
+++ src/day13/index.js
@@ -1,5 +1,112 @@
-const moduleA = input => {
+import {
+  CART_CHOICES,
+  CART_DIRECTIONS,
+  MAP_TILES,
+  INTERSECTION_CHOICES,
+  NEXT_DIRECTION,
+} from './constants';
 
+class Cart {
+  constructor(x, y, direction) {
+    this.x = x;
+    this.y = y;
+    this.direction = direction;
+    this.lastChoice = null;
+  }
+
+  getNextChoice() {
+    const newIndex = (CART_CHOICES.indexOf(this.lastChoice) + 1) % Object.keys(CART_CHOICES).length;
+    this.lastChoice = CART_CHOICES[newIndex];
+    return this.lastChoice;
+  }
+
+  getNextPosition() {
+    const {x, y, direction} = this;
+    switch (direction) {
+      case CART_DIRECTIONS.UP:
+        return {x, y: y - 1};
+      case CART_DIRECTIONS.RIGHT:
+        return {x: x + 1, y};
+      case CART_DIRECTIONS.DOWN:
+        return {x, y: y + 1};
+      case CART_DIRECTIONS.LEFT:
+        return {x: x - 1, y};
+      default:
+        throw new Error(`Unknown direction ${direction}`);
+    }
+  }
+
+  getNextDirection(tile) {
+    const {direction} = this;
+
+    const nextDirection = tile === MAP_TILES.INTERSECTION
+      ? NEXT_DIRECTION[direction][tile][this.getNextChoice()]
+      : NEXT_DIRECTION[direction][tile];
+
+    if (!nextDirection) {
+      throw new Error(`Error getting next direction for current direction \`${direction}\` and tile \`${tile}\``);
+    }
+
+    return nextDirection;
+  }
+}
+
+const parseInput = input => {
+  const map = input
+    .split('\n')
+    .filter(line => !!line)
+    .map(line => line.split(''));
+  const carts = [];
+  for (let y = 0; y < map.length; y = y + 1) {
+    const line = map[y];
+    for (let x = 0; x < line.length; x = x + 1) {
+      if (Object.values(CART_DIRECTIONS).includes(line[x])) {
+        carts.push(new Cart(x, y, line[x]));
+
+        line[x] = (line[x] === CART_DIRECTIONS.UP || line[x] === CART_DIRECTIONS.DOWN)
+          ? '|'
+          : '-';
+      }
+    }
+  }
+
+  return {carts, map};
+};
+
+const sortCartsByPriority = (c1, c2) => {
+  if (c1.y !== c2.y) {
+    return c1.y - c2.y;
+  }
+
+  return c1.x - c2.x;
+};
+
+const tick = (carts, map) => {
+  carts.sort(sortCartsByPriority);
+  carts.forEach((cart, index) => {
+    const {x, y} = cart.getNextPosition();
+    cart.direction = cart.getNextDirection(map[y][x]);
+    cart.x = x;
+    cart.y = y;
+
+    const crashingCart = carts.find((c, i) => c.x === x && c.y === y && i !== index);
+    if (crashingCart) {
+      cart.crash = crashingCart.crash = true;
+    }
+  });
+
+  return carts;
+};
+
+const moduleA = input => {
+  const {carts, map} = parseInput(input);
+
+  let crashingCart;
+  do {
+    crashingCart = tick(carts, map).find(c => !!c.crash);
+  } while (!crashingCart);
+
+  return `${crashingCart.x},${crashingCart.y}`;
 };
 
 const moduleB = input => {
@@ -7,6 +114,12 @@ const moduleB = input => {
 };
 
 export {
+  CART_DIRECTIONS,
+  MAP_TILES,
+  INTERSECTION_CHOICES,
+  Cart,
+  parseInput,
+  tick,
   moduleA,
   moduleB,
 };
